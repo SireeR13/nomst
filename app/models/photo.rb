45acca8e4fc7dd7815a3174aa5462022d378e756
SireeR13 <sireereddy@outlook.com>
2019-09-28 03:40:53 +0000

    back to test email

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -1,7 +1,12 @@
 class Photo < ApplicationRecord
   belongs_to :user
   belongs_to :place
+  after_create :send_photo_email
 
   mount_uploader :picture, PictureUploader
+
+  def send_photo_email
+    NotificationMailer.photo_added(self).deliver_now
+  end
 end
 
