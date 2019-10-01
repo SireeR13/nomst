00b5360e6c8f831dbe2b419e939ea92d2605dfca
SireeR13 <sireereddy@outlook.com>
2019-10-01 20:08:54 +0000

    most recent

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -1,12 +1,6 @@
 class Photo < ApplicationRecord
   belongs_to :user
   belongs_to :place
-  after_create :send_photo_email
 
   mount_uploader :picture, PictureUploader
-
-  def send_photo_email
-    NotificationMailer.photo_added(self).deliver_now
   end
-end
-
